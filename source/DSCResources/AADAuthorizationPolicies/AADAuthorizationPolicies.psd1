694b3b2f6e08e0efe8001d727a7a918300246027
Raimund Andree <r.andree@live.com>
2022-10-10 19:14:14 +0200

    Added configuration 'AADAuthorizationPolicies'

---
+++ source/DSCResources/AADAuthorizationPolicies/AADAuthorizationPolicies.psd1
@@ -0,0 +1,15 @@
+@{
+    RootModule           = 'AADAuthorizationPolicies.schema.psm1'
+
+    ModuleVersion        = '0.0.1'
+
+    GUID                 = '64656a56-0a22-4be0-a6b4-4ca78c8851d7'
+
+    Author               = 'NA'
+
+    CompanyName          = 'NA'
+
+    Copyright            = 'NA'
+
+    DscResourcesToExport = @('AADAuthorizationPolicies')
+}
---
+++ source/DSCResources/AADAuthorizationPolicies/AADAuthorizationPolicies.schema.psm1
@@ -0,0 +1,97 @@
+configuration AADAuthorizationPolicies {
+
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        [ValidateSet('Yes')]
+        $IsSingleInstance,
+
+        [Parameter()]
+        [System.String]
+        $DisplayName,
+
+        [Parameter()]
+        [System.String]
+        $Description,
+
+        [Parameter()]
+        [System.Boolean]
+        $AllowedToSignUpEmailBasedSubscriptions,
+
+        [Parameter()]
+        [System.Boolean]
+        $AllowedToUseSSPR,
+
+        [Parameter()]
+        [System.Boolean]
+        $AllowEmailVerifiedUsersToJoinOrganization,
+
+        [Parameter()]
+        [System.String]
+        [validateset('None', 'AdminsAndGuestInviters', 'AdminsGuestInvitersAndAllMembers', 'Everyone')]
+        $AllowInvitesFrom,
+
+        [Parameter()]
+        [System.Boolean]
+        $BlockMsolPowerShell,
+
+        [Parameter()]
+        [System.Boolean]
+        $DefaultUserRoleAllowedToCreateApps,
+
+        [Parameter()]
+        [System.Boolean]
+        $DefaultUserRoleAllowedToCreateSecurityGroups,
+
+        [Parameter()]
+        [System.Boolean]
+        $DefaultUserRoleAllowedToReadOtherUsers,
+
+        [Parameter()]
+        [System.String[]]
+        $PermissionGrantPolicyIdsAssignedToDefaultUserRole,
+
+        [Parameter()]
+        [validateset('User', 'Guest', 'RestrictedGuest')]
+        [System.String]
+        $GuestUserRole,
+
+        #generic
+        [Parameter()]
+        [ValidateSet('Present')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $ApplicationSecret,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    $param = $PSBoundParameters
+    $param.Remove('InstanceName')
+    (Get-DscSplattedResource -ResourceName AADAuthorizationPolicy -ExecutionName $DisplayName -Properties $param -NoInvoke).Invoke($param)
+}
