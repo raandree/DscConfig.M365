3585424cb9a8d3c57e4af7f2ff4cebe69bd28def
Raimund Andree <r.andree@live.com>
2022-10-11 11:04:30 +0200

    Fixed configuration 'ADGroupSettings'

--- source/DSCResources/ADGroupSettings/ADGroupSettings.schema.psm1
+++ source/DSCResources/ADGroupSettings/ADGroupSettings.schema.psm1
@@ -1,42 +1,73 @@
 configuration ADGroupSettings {
-    param (
-        [Parameter()]
-        [hashtable[]]
-        $Items
-    )
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        [ValidateSet('Yes')]
+        $IsSingleInstance,
 
-<#
-AADGroupsSettings [String] #ResourceName
-{
-    IsSingleInstance = [string]{ Yes }
-    [AllowGuestsToAccessGroups = [bool]]
-    [AllowGuestsToBeGroupOwner = [bool]]
-    [AllowToAddGuests = [bool]]
-    [ApplicationId = [string]]
-    [ApplicationSecret = [string]]
-    [CertificateThumbprint = [string]]
-    [Credential = [PSCredential]]
-    [DependsOn = [string[]]]
-    [EnableGroupCreation = [bool]]
-    [Ensure = [string]{ Absent | Present }]
-    [GroupCreationAllowedGroupName = [string]]
-    [GuestUsageGuidelinesUrl = [string]]
-    [PsDscRunAsCredential = [PSCredential]]
-    [TenantId = [string]]
-    [UsageGuidelinesUrl = [string]]
-}
-#>
+        [Parameter()]
+        [System.Boolean]
+        $EnableGroupCreation,
+
+        [Parameter()]
+        [System.Boolean]
+        $AllowGuestsToBeGroupOwner,
+
+        [Parameter()]
+        [System.Boolean]
+        $AllowGuestsToAccessGroups,
+
+        [Parameter()]
+        [System.String]
+        $GuestUsageGuidelinesUrl,
+
+        [Parameter()]
+        [System.String]
+        $GroupCreationAllowedGroupName,
+
+        [Parameter()]
+        [System.Boolean]
+        $AllowToAddGuests,
+
+        [Parameter()]
+        [System.String]
+        $UsageGuidelinesUrl,
+
+        [Parameter()]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter()]
+        [System.String]
+        $ApplicationId,
+
+        [Parameter()]
+        [System.String]
+        $TenantId,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]
+        $ApplicationSecret,
+
+        [Parameter()]
+        [System.String]
+        $CertificateThumbprint,
+
+        [Parameter()]
+        [Switch]
+        $ManagedIdentity
+    )
 
     Import-DscResource -ModuleName PSDesiredStateConfiguration
     Import-DscResource -ModuleName Microsoft365DSC
 
-    foreach ($item in $Items)
-    {
-        if (-not $item.ContainsKey('Ensure'))
-        {
-            $item.Ensure = 'Present'
-        }
-
-        (Get-DscSplattedResource -ResourceName AADGroupsSettings -ExecutionName AADGroupsSettingsInstance -Properties $item -NoInvoke).Invoke($item)
-    }
+    $param = $PSBoundParameters
+    $param.Remove('InstanceName')
+    (Get-DscSplattedResource -ResourceName AADGroupsSettings -ExecutionName AADGroupsSettingsInstance -Properties $param -NoInvoke).Invoke($param)
 }
